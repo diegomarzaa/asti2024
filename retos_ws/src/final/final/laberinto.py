36ce13f728c7d480586be93fde52f60cd4828ad5
diegomarzaa <al426641@uji.es>
2024-04-12 23:38:09 +0200

    Update laberinto.py

--- retos_ws/src/final/final/laberinto.py
+++ retos_ws/src/final/final/laberinto.py
@@ -32,13 +32,13 @@ def pruebas(mov, sensors):
 def basico(mov, sensors):
     distancias = sensors.distancias()
     if distancias[1] > 5 and distancias[2] > 5:
-        if round(distancia[3],1) < 10:    # Demasiado cerca de la paredes
+        if round(distancias[3],1) < 10:    # Demasiado cerca de la paredes
             mov.girar_der(10)
-        elif round(distancia[3], 1) > 10 and distancia[3] < 30:
+        elif round(distancias[3], 1) > 10 and distancias[3] < 30:
             mov.girar_izq(8)
         else:
             mov.avanzar()
-        if distancia[3] > 30:
+        if distancias[3] > 30:
             mov.girar_grados_der(90)
     else:
         while round(distancias[2], 1) == round(distancias[1], 1) and distancias[1] < 25:
@@ -51,8 +51,8 @@ def basico(mov, sensors):
     
 def paralelo_paredes(mov, sensors): # Cambia el valor de las ruedas según la distancia a las paredes 
     distancias = sensors.distancias()
-    diferencia = distancia[3] - distancia[0] # distancia entre derecha e izquierda
-    if distancia[2] < 5 and distancia[1] < 5:
+    diferencia = distancias[3] - distancias[0] # distancia entre derecha e izquierda
+    if distancias[2] < 5 and distancias[1] < 5:
         while round(distancias[2], 1) == round(distancias[1], 1) and distancias[1] < 5:
             mov.actualizar_vel_lineal(0.0)
             mov.actualizar_vel_angular(-0.1)
@@ -63,12 +63,12 @@ def paralelo_paredes(mov, sensors): # Cambia el valor de las ruedas según la di
         mov.actualizar_vel_angular(diferencia/FACTOR)    # Revisar signo y factor
     elif diferencia > 0:                            # Camino derecha
         mov.girar_grados_der(90, 0.2)
-    elif distancia[1] < 25 and diferencia < 0:     # Única posibilida izquierda
+    elif distancias[1] < 25 and diferencia < 0:     # Única posibilida izquierda
         mov.girar_grados_izq(90, 0.2)
     else:
         mov.girar_grados_der(180)
 
-def mano_derecho(mov, sensors):
+def mano_derecha(mov, sensors):
     mov.avanzar_derecha(mov, sensors)
 
 
