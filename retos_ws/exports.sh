0adce32d2d11964573ffd54bb41ff17fa9c675b1
Jcrex <al428669@uji.es>
2024-04-12 10:31:56 +0200

    export.sh

--- retos_ws/exports.sh
+++ retos_ws/exports.sh
@@ -1,4 +1,29 @@
 #! /bin/bash
 
+source /opt/ros/foxy/setup.bash
+source install/setup.bash
+
 export ROS_LOCALHOST_ONLY=0
 export ROS_DOMAIN_ID=30
+
+#!/bin/bash
+
+lxterminal -e '/bin/bash -c "source /opt/ros/foxy/setup.bash;
+                source install/setup.bash; export ROS_LOCALHOST_ONLY=0;
+                export ROS_DOMAIN_ID=30;
+                exec bash"'
+lxterminal -e '/bin/bash -c "source /opt/ros/foxy/setup.bash;
+                source install/setup.bash;
+                export ROS_LOCALHOST_ONLY=0;
+                export ROS_DOMAIN_ID=30;
+                exec bash"'
+lxterminal -e '/bin/bash -c "source /opt/ros/foxy/setup.bash;
+                source install/setup.bash;
+                export ROS_LOCALHOST_ONLY=0;
+                export ROS_DOMAIN_ID=30;
+                exec bash"'
+lxterminal -e '/bin/bash -c "source /opt/ros/foxy/setup.bash;
+                source install/setup.bash;
+                export ROS_LOCALHOST_ONLY=0;
+                export ROS_DOMAIN_ID=30;
+                exec bash"'
