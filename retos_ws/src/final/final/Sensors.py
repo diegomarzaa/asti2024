82f02e117ecd88a3a91383b67999dae55e4c283f
guillemfustier <al426661@uji.es>
2024-04-13 06:00:07 +0200

    a la primera?

--- retos_ws/src/final/final/Sensors.py
+++ retos_ws/src/final/final/Sensors.py
@@ -5,6 +5,8 @@ from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
 from final.Movements import Movements
 
+DISTANCIA_ENTRE_SENSORES = 10.7
+
 class Sensors(Node):
     def __init__(self):
         super().__init__('distance_sensor_subscriber')
@@ -12,6 +14,16 @@ class Sensors(Node):
         self.array_subscriber_ = self.create_subscription(Float32MultiArray, '/distancias_sensores', self.array_callback, 10)
 
     def array_callback(self, msg):
+
+        def girar_pared_diagonal(mov, sensor_del_izq, sensor_del_der):
+            y = abs(sensor_del_izq - sensor_del_der)
+            x = DISTANCIA_ENTRE_SENSORES
+            angulo_giro = math.atan(y/x)
+            angulo_giro = angulo_giro * 180 / math.pi  
+            if distancia_delante_der < distancia_delante_izq:
+                mov.girar_grados_izq(angulo_giro)
+            else:
+                mov.girar_grados_der(angulo_giro)
 
         def detectar_pared(sensor_del_izq, sensor_del_der, dist_pared = 40):
             print(f"Distancia delante izq: {sensor_del_izq}, Distancia delante der: {sensor_del_der}")
@@ -25,58 +37,55 @@ class Sensors(Node):
                 mov.detener()
             else:
                 mov.avanzar_distancia(0.05)
+
+        def detectar_izquierda_libre(sensor_izq, dist_izq_libre = 60):
+            if sensor_izq > dist_izq_libre:
+                return True
+            else:
+                return False
+
+        def detectar_derecha_libre(sensor_der, dist_der_libre = 60):
+            if sensor_der > dist_der_libre:
+                return True
+            else:
+                return False
+
+        def avanzar_hasta_pared_izquierda(mov, sensor_izq):
+            while True:
+                if detectar_izquierda_libre(sensor_izq):
+                    break
+                mov.avanzar_distancia(0.05)
+            mov.detener()
             
         self.get_logger().info('Received array: ' + str(msg.data))
 
         mov = Movements()
         mov.actualizar_vel_lineal(0.15)
-        distancia_der = msg.data[0]
+        distancia_izq = msg.data[0]
         distancia_delante_izq = msg.data[1]
         distancia_delante_der = msg.data[2]
         distancia_der = msg.data[3]
         
-        avanzar_hasta_pared(mov, distancia_delante_izq, distancia_delante_der)
+        mov.avanzar_distancia(0.05)
+        print("holaaaaa")
+        if mov.detectar_izquierda_libre(distancia_izq):
+            print("izquierda libre")
+            mov.detener()
+            mov.girar_izq(90)
+            time.sleep(1)
+        elif mov.detectar_pared(distancia_delante_izq, distancia_delante_der):
+            print("pared")
+            mov.detener()
+            if abs(distancia_delante_izq - distancia_delante_der) < 5:
+                if distancia_izq > distancia_der:
+                    mov.girar_izq(90)
+                else:
+                    mov.girar_der(90)
+            else:
+                girar_pared_diagonal(mov, distancia_delante_izq, distancia_delante_der)
+            time.sleep(1)
         
-    def get_distancia_derecha(self):
-        return self.distancia_der
-    
-    def get_distancia_izquierda(self):
-        return self.distancia_izq
 
-    def get_distancia_delante_der(self):
-        return self.distancia_delante_der
-    
-    def get_distancia_delante_izq(self):
-        return self.distancia_delante_izq
-    
-    def get_distancias(self):
-        """
-        Return: tupla izquierda, delante_izq, delante_der, derecha
-        """
-        izquierda = self.get_distancia_izquierda()
-        delante_izq = self.get_distancia_delante_izq()
-        delante_der = self.get_distancia_delante_der()
-        derecha = self.get_distancia_derecha()
-        return (izquierda, delante_izq, delante_der, derecha)
-    
-    def principal(self):
-        mov = Movements()
-        while True:
-            op = input("Introduce una opción (1-pruebas/2-salir): ")
-            if op == '1':
-                print("Pruebas")
-                mov.detectar_pared(self)
-            elif op == '2':
-                break
-
-    def detectar_pared(self, dist_pared = 20):
-        dist_delante_izq = self.distancia_delante_izq()
-        dist_delante_der = self.distancia_delante_der()
-        print(f"Distancia delante izq: {dist_delante_izq}, Distancia delante der: {dist_delante_der}")
-        if (dist_delante_izq < dist_pared) or (dist_delante_der < dist_pared):
-            return True
-        else:
-            return False
 
 def main(args=None):
     rclpy.init(args=args)
