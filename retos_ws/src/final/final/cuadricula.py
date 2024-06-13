9f07371291d890933f4b03af62419ebd92a2f320
guillemfustier <al426661@uji.es>
2024-06-13 15:51:11 +0200

    medidas ofi

--- retos_ws/src/final/final/cuadricula.py
+++ retos_ws/src/final/final/cuadricula.py
@@ -9,7 +9,7 @@ from final.Movements import Movements, pedir_velocidades
 ERROR_ANGULO = 0.0        # TODO: Comprobar cual es el error sistematico
 DISTANCIA_EXTRA = 0.2
 
-
+"""
 DISTANCIA_TABLERO_COL = 2.87   # Suma de las distancias de las columnas
 DISTANCIA_TABLERO_FILA = 2.83 # Suma de las distancias de las filas
 
@@ -18,6 +18,16 @@ COL_LETRAS = [0.34, 0.11, 0.63, 0.52, 0.46, 0.24, 0.23, 0.34]
 
 FILA_NUM = [0.40, 0.33, 0.50, 0.14, 0.745, 0.215, 0.50]
 FILA_LETRAS = [0.40, 0.33, 0.50, 0.14, 0.745, 0.215, 0.50]
+"""
+
+DISTANCIA_TABLERO_COL = 2.47   # Suma de las distancias de las columnas
+DISTANCIA_TABLERO_FILA = 2.43 # Suma de las distancias de las filas
+
+COL_NUM = [0.23, 0.46, 0.20, 0.26, 0.17, 0.53, 0.35, 0.24]
+COL_LETRAS = [0.23, 0.46, 0.20, 0.26, 0.17, 0.53, 0.35, 0.24]
+
+FILA_NUM = [0.37, 0.215, 0.58, 0.38, 0.515, 0.22, 0.15]
+FILA_LETRAS = [0.37, 0.215, 0.58, 0.38, 0.515, 0.22, 0.15]
 
 # INICIO - NÚMEROS
 diccionario_numeros = {
