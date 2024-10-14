12da32073544431ea32f00ea110010e363fed882
diegomarzaa <diegomarzafuertes@gmail.com>
2024-10-14 12:52:16 +0200

    network sharing fix

--- abrir-workspace-asti.sh
+++ abrir-workspace-asti.sh
@@ -4,8 +4,10 @@ xhost +local:root   # Dar permisos para que se puedan visualizar ventanas como e
 
 # -it: Para que puedas usar el contenedor directamente desde la terminal    # --rm: Para no dejar contenedores abiertos que ocupen memoria
 # Montamos la carpeta `asti2024_ws` en tu máquina al contenedor, para que los cambios que hagas en el código dentro del contenedor se guarden en tu máquina
+# --network host: Para que el contenedor pueda acceder a la red de tu máquina
 
 sudo docker run -it --rm \
+    --network host \
     --name asti_container \
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
